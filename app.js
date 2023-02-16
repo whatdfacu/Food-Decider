78bd7f3321a991d01d098e05afd3aa78859945c5
whatdfacu <75960995+whatdfacu@users.noreply.github.com>
2023-02-16 17:07:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ decideButton.addEventListener("click", function() {
   document.getElementById("food-options").classList.remove("hidden");
   result.classList.add("hidden");
 });
+
+for (let i = 0; i < foodOptions.length; i++) {
+  foodOptions[i].addEventListener("click", function() {
+    result.innerHTML = `You chose ${foodOptions[i].innerText}`;
+    result.classList.remove("hidden");
+    document.getElementById("food-options").classList.add("hidden");
+  });
+}
